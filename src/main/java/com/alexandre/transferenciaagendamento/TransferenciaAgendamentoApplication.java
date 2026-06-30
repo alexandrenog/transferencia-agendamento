18292df34e1b023e94475986162dab3a1e690af6
alexandrenogueira <alexandre.nog212@gmail.com>
2026-06-30 11:35:04 -0300

    Commit inicial

---
+++ src/main/java/com/alexandre/transferenciaagendamento/TransferenciaAgendamentoApplication.java
@@ -0,0 +1,13 @@
+package com.alexandre.transferenciaagendamento;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TransferenciaAgendamentoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TransferenciaAgendamentoApplication.class, args);
+    }
+
+}
